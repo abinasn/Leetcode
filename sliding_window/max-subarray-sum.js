ce7b4cbddd238e34d47849152bfdd23556cb2fc6
Abinas Patra <2010abinas@gmail.com>
2025-01-20 21:08:57 +0530

    sliding window

---
+++ sliding_window/max-subarray-sum.js
@@ -0,0 +1,15 @@
+var maxSubarraySum = function(arr, n){
+    let sum=0, max=0;
+    for(let i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    max= sum;
+    for(let i=n;i<arr.length;i++){
+        sum = (sum - arr[i-n]) + arr[i];
+        max = Math.max(max,sum)
+    }
+    return max;
+}
+
+console.log(maxSubarraySum([1,2,5,2,8,1,5],2))
+console.log(maxSubarraySum([1,2,5,2,8,1,5],4))
